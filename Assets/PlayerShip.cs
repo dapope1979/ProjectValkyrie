fca6e5ff40475203dc339b38572df570ed8e150d
Nathan <dapope1979@gmail.com>
2013-02-07 21:25:19 -0700

    Some renaming and configuring to get movement

--- Assets/PlayerShip.cs
+++ Assets/PlayerShip.cs
@@ -11,7 +11,7 @@ public class PlayerShip : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (moving) {
-			Vector3 moveDir = new Vector3(0, 1, 0);
+			Vector3 moveDir = new Vector3(0, 0, 1);
 			float speed = 5;
 	    	transform.Translate(speed * moveDir * Time.deltaTime);
 		}
